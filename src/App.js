01c0b50ee93b9992721568a6989f813ac42240eb
Ziqi Wang <ziqi.wang39@gmail.com>
2021-09-17 18:43:03 +1000

    Add todo list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Review Mongoose" />
+      <Todo text="Review React.js" />
+      <Todo text="Learn sth else" />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 // import UseRef from "./components/UseRef/UseRef";
-import SearchFilter from "./components/SearchFilter/SearchFilter.jsx";
+// import SearchFilter from "./components/SearchFilter/SearchFilter.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App />
-    <UseRef /> */}
-    <SearchFilter />
+    <App />
+    {/* <UseRef />
+    <SearchFilter /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
